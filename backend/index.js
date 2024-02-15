a0628550b7be6c9426785eaef87b0857f554d984
Natannms <agnusnat.nms@gmail.com>
2024-02-15 18:29:54 -0300

    Estado inicial do projeto

---
+++ backend/index.js
@@ -0,0 +1,88 @@
+const cors = require("cors");
+const express = require("express");
+require('dotenv').config()
+const validator = require('validator');
+const mysql = require('mysql2/promise');
+const app = express();
+const PORT =  process.env.PORT
+const dbConfig = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password:process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const { email } = req.body;
+
+  if (validator.isEmail(email)) {
+    const sanitizedEmail = validator.normalizeEmail(email);
+
+    try {
+      const connection = await mysql.createConnection(dbConfig);
+      const [results, fields] = await connection.execute(
+        'INSERT INTO emails (email) VALUES (?)',
+        [sanitizedEmail]
+      );
+      connection.end();
+
+      res.send('E-mail inserido no banco de dados: ' + sanitizedEmail);
+    } catch (error) {
+      console.error('Erro ao inserir o e-mail no banco de dados:', error);
+      res.status(500).send('Erro interno ao inserir o e-mail no banco de dados');
+    }
+  } else {
+    res.status(400).send('Formato de e-mail inválido');
+  }
+});
+
+
+app.get("/emails", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const [results, fields] = await connection.execute('SELECT * FROM emails');
+    connection.end();
+
+    res.send({ emails: results });
+
+  } catch (error) {
+    console.error('Erro ao obter os e-mails:', error);
+    res.status(500).send('Erro interno ao obter os e-mails');
+  }
+});
+
+async function createDatabaseAndTableIfNotExists() {
+  try {
+    const connectionWithoutDB = await mysql.createConnection({
+      host: dbConfig.host,
+      user: dbConfig.user,
+      password: dbConfig.password,
+    });
+
+    await connectionWithoutDB.query('CREATE DATABASE IF NOT EXISTS redway');
+
+    connectionWithoutDB.end();
+
+    const connection = await mysql.createConnection(dbConfig);
+
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS emails (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL
+      )
+    `);
+
+    connection.end();
+  } catch (error) {
+    console.error('Erro ao criar o banco de dados ou tabela:', error);
+  }
+}
+
+app.listen(PORT, async () => {
+  await createDatabaseAndTableIfNotExists();
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+});
---
+++ redApp/src/App.tsx
@@ -0,0 +1,53 @@
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+
+interface Values {
+  email: string;
+}
+
+function App() {
+  // async function setEmail() {
+  //   const response = await fetch('http://localhost:4000')
+  //   const data = await response.json()
+  //   setMessage(data.message)
+  // }
+
+  // useEffect(() => {
+  //   getName()
+  // }, [])
+  return (
+    <div className='bg-gray-800 w-screen h-screen py-20'>
+      <Formik
+        initialValues={{
+          email: '',
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form className="max-w-sm mx-auto">
+          <label htmlFor="email-address-icon" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu email</label>
+          <div className="flex flex-col gap-4">
+            <Field 
+              type="email"
+              name="email"
+              id="email-address-icon" 
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              placeholder="Email@example.com" 
+            />
+            <button type='submit' className='bg-indigo-500 text-white py-2 rounded-md'>
+              Save
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  )
+}
+
+export default App
